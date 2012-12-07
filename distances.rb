7b1c1296b85d40eea8a6b59ff2789b895ecccf1a
HarnoRanaivo <kshegzyaj@gmail.com>
2012-12-07 03:50:21 +0100

    changement classe point vers hash

--- distances.rb
+++ distances.rb
@@ -1,14 +1,5 @@
-class Point
-    attr_accessor :x
-    attr_accessor :y
-    def initialize(x, y)
-        @x = x
-        @y = y
-    end
-end
-
 def distance(p1, p2)
-    ((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2) ** 0.5
+    ((p2[:x] - p1[:x]) ** 2 + (p2[:y] - p1[:y]) ** 2) ** 0.5
 end
 
 # pre : t.size >= 2
@@ -28,7 +19,7 @@ def randompoint(n)
     tab = []
     for i in 0...n
         r = Random.new()
-        tab.push(Point.new(r.rand*10, r.rand*10))
+        tab.push({ :x => (r.rand*10), :y => (r.rand*10) })
     end
     return tab
 end
